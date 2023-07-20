6c335b9728b2a85130301169bb8258fa8fd6a771
Lnola <nola.luka@gmail.com>
2023-07-20 14:00:40 +0200

    Create start dev script :sparkles:

---
+++ avanti/start_dev.sh
@@ -0,0 +1,53 @@
+# !/bin/bash
+
+osascript<<EOF
+    tell application "iTerm"
+        activate
+        select first window
+
+        # Create new tab
+        tell current window
+            create tab with default profile
+        end tell
+
+        # Split pane
+        tell current session of current window
+            split vertically with default profile
+        end tell
+
+        # Split pane
+        tell current session of current window
+            split vertically with default profile
+        end tell
+
+        # Start test postgres in Pane-3
+        tell third session of current tab of current window
+            write text "z sol"
+            write text "av_kill_postgres_test"
+            write text "av_start_postgres_dev"
+        end tell
+
+         # Wait for postgres to start before starting the apps
+        set var_wait to 0
+        repeat until (var_wait = 1)
+            if (text of third session of current tab of current window contains "avanti_postgres") then
+                set var_wait to 1
+            end if
+        end repeat
+
+        # Run command in Pane-1
+        tell first session of current tab of current window
+            write text "z sol"
+            write text "npm run dev:server"
+        end tell
+
+        # Run command in Pane-2
+        tell second session of current tab of current window
+            write text "z sol"
+            write text "npm run dev:client"
+        end tell
+        
+        # Focus Pane-3
+        select third session of current tab of current window
+    end tell
+EOF
