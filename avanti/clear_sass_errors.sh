f5970bf75c46050f71f2c25465b7c8efec94b687
Lnola <nola.luka@gmail.com>
2023-07-20 19:41:18 +0200

    Fix sass migration arguments :bug:

--- avanti/clear_sass_errors.sh
+++ avanti/clear_sass_errors.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check the current working directory matches the location
+location="/Projects/solution-tree-platform"
+if [ "$(pwd)" != "$HOME$location" ]; then
+    echo "Error: Script must be run from the ~$location folder."
+    exit 1
+fi
+
 # Check if folder path and file extension are provided as arguments
 if [ -z "$1" ] || [ -z "$2" ]; then
   echo "Usage: $0 <folder_path> <file_extension>"
@@ -12,7 +19,7 @@ file_extension="$2"
 
 # Perform the sass-migrator conversion for all defined files
 npm install -g sass-migrator
-sass-migrator division $folder_path/**/*.$file_extension
+sass-migrator division --verbose --migrate-deps $folder_path/**/*.$file_extension
 
 # Step 1: Remove any mention of "@use "sass:math""
 # Step 2: Replace all occurrences of "math.div(x, y)" with "calc(x / y)"
