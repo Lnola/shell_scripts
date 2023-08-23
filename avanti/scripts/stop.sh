24986cc928fec2a45cbb3c6589520c7d5e6318d8
Lnola <nola.luka@gmail.com>
2023-08-24 00:56:41 +0200

    :sparkles: Create a stop script

---
+++ avanti/scripts/stop.sh
@@ -0,0 +1,23 @@
+# !/bin/bash
+
+osascript<<EOF
+tell application "iTerm"
+        activate
+        select first window
+
+        tell first session of current tab of current window
+            write text ""
+            write text ""
+        end tell
+
+        tell second session of current tab of current window
+            write text ""
+            write text ""
+        end tell
+
+        tell current tab of current window
+            close
+        end tell
+    end tell
+EOF
+
