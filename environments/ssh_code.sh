ea472d8169a97fe1aa128a3d02e04028844b1558
Lnola <nola.luka@gmail.com>
2023-10-12 21:56:29 +0200

    :sparkles: Create an open vscode with ssh script

---
+++ environments/ssh_code.sh
@@ -0,0 +1,20 @@
+# !/bin/bash
+
+osascript<<EOF
+    tell application "Visual Studio Code"
+        activate
+        delay 1
+        tell application "System Events"
+            key down command
+            keystroke "p"
+            key up command
+            keystroke ">Remote-SSH: Connect Current Window to Host"
+            delay 1
+            key code 36
+            delay 1
+            keystroke "localhost"
+            delay 1
+            key code 36
+        end tell
+    end tell
+EOF
