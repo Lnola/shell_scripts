76cdbea616bf8a2961b7120368f90d31746327d4
Lnola <nola.luka@gmail.com>
2023-10-13 02:32:01 +0200

    :zap: Move wait to only current session

--- environments/python_run.sh
+++ environments/python_run.sh
@@ -15,17 +15,15 @@ osascript<<EOF
             write text "(z python && docker compose up -d)"
             # SSS into the docker container
             write text "ssh root@localhost -p 2222"
-        end tell
 
-        #  Wait for Command + D
-        set var_wait to 0
-        repeat until (var_wait = 1)
-            if (text of current session of current window contains "Connection to localhost closed.") then
-                set var_wait to 1
-            end if
-        end repeat
+            #  Wait for Command + D
+            set var_wait to 0
+            repeat until (var_wait = 1)
+                if (text of current session of current window contains "Connection to localhost closed.") then
+                    set var_wait to 1
+                end if
+            end repeat
 
-        tell current session of current window
             # Stop the docker container
             write text "(z python && docker compose stop)"
         end tell
