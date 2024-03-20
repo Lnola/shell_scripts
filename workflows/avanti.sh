a9ceb1f77522ac6a4d65a09309d984ae9d22e815
Lnola <nola.luka@gmail.com>
2024-03-20 16:53:18 +0100

    :sparkles: Create a simple avanti workflow

---
+++ workflows/avanti.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.packageName Workflows
+# @raycast.title avanti
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ./assets/avanti.png
+
+# Documentation:
+# @raycast.description Start avanti workflow
+# @raycast.author Luka Nola
+
+
+osascript<<EOF
+    tell application "Arc" to activate
+    tell application "Slack" to activate
+    tell application "Spotify" to activate
+    tell application "Iterm" to activate
+
+    delay 3
+    do shell script "utils/move_app_to_space.sh Spotify 1"
+    do shell script "utils/move_app_to_space.sh Slack 2"
+    do shell script "utils/move_app_to_space.sh Iterm 5"
+    delay 1
+    do shell script "utils/move_app_to_space.sh Arc 3"
+EOF
