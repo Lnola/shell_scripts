d22a8770ca866fdba691a78a69d496554b0b83fb
Lnola <nola.luka@gmail.com>
2024-03-20 17:04:13 +0100

    :sparkles: Add iterm project setup routine

--- workflows/avanti.sh
+++ workflows/avanti.sh
@@ -20,10 +20,44 @@ osascript<<EOF
     tell application "Spotify" to activate
     tell application "Iterm" to activate
 
-    delay 3
+    delay 2
+
     do shell script "utils/move_app_to_space.sh Spotify 1"
     do shell script "utils/move_app_to_space.sh Slack 2"
     do shell script "utils/move_app_to_space.sh Iterm 5"
+
     delay 1
+
     do shell script "utils/move_app_to_space.sh Arc 3"
+
+    delay 1
+
+    tell application "Iterm"
+        activate
+        write text "z sol && code ."
+
+        # Split pane
+        tell current session of current window
+            split vertically with default profile
+        end tell
+
+        # Split pane
+        tell current session of current window
+            split horizontally with default profile
+        end tell
+
+        tell first session of current tab of current window
+            write text "z learn"
+            write text "npm run storybook:dev"
+        end tell
+
+        tell second session of current tab of current window
+            write text "z learn"
+            write text "npm run dev"
+        end tell
+
+        tell third session of current tab of current window
+            write text "z learn && code ."
+        end tell
+    end tell
 EOF
