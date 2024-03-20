a903790087dc8e3b8b34c87a99fd83382357e254
Lnola <nola.luka@gmail.com>
2024-03-20 16:04:13 +0100

    :truck: Move throw space function to the file

--- workflows/amethyst/throw-space-n.sh
+++ workflows/amethyst/throw-space-n.sh
@@ -14,5 +14,12 @@
 # @raycast.description Move the currently focused app to the nth space
 # @raycast.author Luka Nola
 
-"../utils/move_app_to_space.sh" "$1"
+# Get the AppleScript keycode from the argument
+keycodes=( 18 19 20 21 23 22 26 28 25 )
+keycode=${keycodes[$1 - 1]}
 
+osascript<<EOF
+    tell application "System Events" 
+        key code $keycode using {shift down, option down, control down}
+    end tell
+EOF
