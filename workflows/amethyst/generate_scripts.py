fb42886d8deb70a76099e2e97e0ddf4be9d4fe75
Lnola <nola.luka@gmail.com>
2024-03-20 15:10:20 +0100

    :recycle: Upgrade script with security and modifier mapping

--- workflows/amethyst/generate_scripts.py
+++ workflows/amethyst/generate_scripts.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-import os, yaml
+import os, yaml, re
 
 home_dir = os.environ['HOME']
 config_file_path = f"{home_dir}/.amethyst.yml"
-mod1 = ['option', 'shift']
-mod2 = ['option', 'shift', 'control']
-keycodes = {
+
+modifiers_mapping = {}
+
+key_codes = {
     'enter': 36,
     'return': 36,
     'space': 49,
@@ -13,6 +14,16 @@ keycodes = {
     'right': 124,
     'down': 125,
     'up': 126,
+    '1': 18,
+    '2': 19,
+    '3': 20,
+    '4': 21,
+    '5': 23,
+    '6': 22,
+    '7': 26,
+    '8': 28,
+    '9': 25,
+
 }
 
 script_template = """#!/usr/bin/osascript
@@ -20,95 +31,86 @@ script_template = """#!/usr/bin/osascript
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.packageName Amethyst
-# @raycast.title Generate amethyst scripts
+# @raycast.title {title}
 # @raycast.mode compact
-
 # Optional parameters:
 # @raycast.icon ./assets/amethyst.png
 # @raycast.author Luka Nola
 # @raycast.description Keyboard shortcuts automatic generator from config .amethyst.yml
 
-tell application "System Events" 
-    <<command>> using {<<mods>>}
+tell application "System Events"
+    {key_command} using {{{mod_keys}}}
 end tell
 """
 
-# mod1|mod2 -> option down, shift down
-def mods_csv(mod):
-    if mod != 'mod1' and mod != 'mod2':
-        raise ValueError(f"invalid mod: {mod}")
-
-    mods = mod1 if mod == 'mod1' else mod2
-    return ', '.join([f"{mod} down" for mod in mods])
-
-
-# h, j, k, l, space, return, left, right -> keystroke "h" | key code 49
-def key_cmd(key):
-    if len(key) == 1:
-        return f"keystroke \"{key}\""
-    elif key in keycodes:
-        code = keycodes[key]
-        return f"key code {code}"
-    else:
-        raise ValueError(f"invalid key: {key}")
-
-
-def to_sentence(name):
-    title = name.replace('-', ' ')
-    title = title.replace('ccw', 'CCW')
-    title = title.replace('cw', 'CW')
-    title = title[0].upper() + title[1:]
-    return title
-
-
-def generate_script(name, mod, key):
+def load_yaml_config(file_path):
+    """Load and return YAML configuration from a file."""
     try:
-        title = to_sentence(name)
-        mods = mods_csv(mod)
-        command = key_cmd(key)
-        script = script_template.replace('<<title>>', title).replace('<<command>>', command).replace('<<mods>>', mods)
-        
-        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-        filepath = os.path.join(__location__, f"{name}.applescript")
-        with open(filepath, 'w') as f:
-            f.write(script)
-    except ValueError:
-        print(f"Unable to generate script for key (name: {name}, mod: {mod}, key: {key})")
-
-
-def generate_scripts(data):
-    if isinstance(data, dict):
-        for key, value in data.items():
-            if isinstance(value, dict) and value.get('key') is not None and value.get('mod') is not None:
-                print(f"Generating script for {key}")
-                generate_script(key, value['mod'], value['key'])
-
-
-def parse_config_file(file_path):
-    try:
-        with open(file_path, "r") as f:
-            config_data = yaml.safe_load(f)  
-            # If the config file is empty exit.
+        with open(file_path, "r") as file:
+            config_data = yaml.safe_load(file)
             if config_data is None:
                 print("The configuration file is empty or incorrectly formatted.")
                 exit(3)
             return config_data
 
     except FileNotFoundError:
-        print(f"Error: Config file not found at {file_path}.")
+        print(f"Error: Config file not found at {file_path}. Exiting.")
         exit(1)
 
     except yaml.YAMLError as exc:
-        print(f"Error parsing YAML file: {exc}")
+        print(f"Error parsing YAML file: {exc}. Exiting.")
         exit(2)
 
 
-def main():
-    # Parse the configuration file.
-    config_data = parse_config_file(config_file_path) 
+def format_title(name):
+    """Convert snake_case or kebab-case to Title Case."""
+    return name.replace('-', ' ').replace('_', ' ').title().replace(' Ccw', ' CCW').replace(' Cw', ' CW')
+
+
+def generate_applescript_commands(mods, key):
+    """Generate AppleScript commands from modifiers and key."""
+    if mods not in modifiers_mapping:
+        raise ValueError(f"Invalid modifier: {mods}")
     
-    # Generate scripts based on the parsed YAML data.
-    generate_scripts(config_data) 
+    mod_keys = ', '.join([f"{mod} down" for mod in modifiers_mapping[mods]])
+    
+    if len(key) == 1:
+        key_command = f'keystroke "{key}"'
+    elif key in key_codes:
+        key_command = f'key code {key_codes[key]}'
+    else:
+        raise ValueError(f"Invalid key: {key}")
+    
+    return mod_keys, key_command
+
+
+def generate_script(file_path, title, mod_keys, key_command):
+    """Generate AppleScript based on the template and write to the file."""
+    script_content = script_template.format(title=title, key_command=key_command, mod_keys=mod_keys)
+    with open(file_path, 'w') as script_file:
+        script_file.write(script_content)
+
+
+def generate_scripts(name, attributes):
+    try:
+        title = format_title(name)
+        mod_keys, key_command = generate_applescript_commands(attributes['mod'], attributes['key'])
+        script_path = f"{name}.applescript"
+        generate_script(script_path, title, mod_keys, key_command)
+        print(f"Generated script: {script_path}")
+    except ValueError as error:
+        print(f"Skipping '{name}': {error}")
+
+
+def main():
+    config_data = load_yaml_config(config_file_path)
+
+    for key, value in config_data.items():
+        if re.match(r'^mod[1-4]', key):
+            modifiers_mapping[key] = value
+
+        if isinstance(value, dict):
+            generate_scripts(key, value)
 
 if __name__ == "__main__":
     main()
