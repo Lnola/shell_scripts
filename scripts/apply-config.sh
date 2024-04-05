f4d49f2bbdeffd9150d86b3af5accce95edebdb2
Lnola <nola.luka@gmail.com>
2024-04-05 02:01:15 +0200

    ⚡️ Update the apply config script to read a json file instead of a .env

--- scripts/apply-config.sh
+++ scripts/apply-config.sh
@@ -3,26 +3,35 @@
 # Path to the previously created script
 COPY_SCRIPT_PATH="$HOME/bin/scripts/copy-file-to-location.sh"
 
-# Check if the .env file path is provided
+# Check if the JSON file path is provided
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <path_to_env_file>"
+    echo "Usage: $0 <path_to_json_file>"
     exit 1
 fi
 
-ENV_FILE="$1"
+JSON_FILE="$1"
+# Determine the directory of the JSON file
+JSON_FILE_DIR=$(dirname "$(realpath "$JSON_FILE")")
 
-# Check if the .env file exists
-if [ ! -f "$ENV_FILE" ]; then
-    echo "The .env file does not exist."
+# Check if the JSON file exists
+if [ ! -f "$JSON_FILE" ]; then
+    echo "The JSON file does not exist."
     exit 2
 fi
 
-# Read each line from the .env file
-while IFS='=' read -r src dst; do
-    # Check if src and dst are not empty
+# Process each source and destination from the JSON file
+length=$(jq '. | length' "$JSON_FILE")
+for ((i = 0 ; i < length ; i++)); do
+    src=$(jq -r ".[$i].source" "$JSON_FILE")
+    dst=$(jq -r ".[$i].destination" "$JSON_FILE")
+    
+    # Resolve full paths
+    src_abs="$JSON_FILE_DIR/$src"
+    dst_abs="$JSON_FILE_DIR/$dst"
+    
     if [[ -n "$src" && -n "$dst" ]]; then
-        echo "Copying $src to $dst..."
-        # Call the copy script with source file and destination directory
-        "$COPY_SCRIPT_PATH" "$src" "$dst"
+        echo "Copying $src_abs to $dst_abs..."
+        # Call the copy script with absolute source and destination paths
+        "$COPY_SCRIPT_PATH" "$src_abs" "$dst_abs"
     fi
-done < "$ENV_FILE"
+done
