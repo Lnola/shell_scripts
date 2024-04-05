361b29dfe3da8ade1a5e46aa93a7684d2b7c8a2a
Lnola <nola.luka@gmail.com>
2024-04-05 04:12:18 +0200

    💡 Add a script explanation and warning to all config files

---
+++ config/.zsh_config/.color_config.p10k.zsh
@@ -0,0 +1,7 @@
+# WARNING: DO NOT CHANGE MANUALLY!
+# USE THE ~/bin FOLDER INSTEAD
+
+# To apply the changes manually run
+# $ copy-file-to-destination --with-check ~/bin/config/.zsh_config/.color_config.p10k.zsh ~/.zsh_config
+
+# TODO: decide what to do with this
