632d4a0151b2a242b4caaf316ae1318bcc3daf72
Lnola <nola.luka@gmail.com>
2024-04-17 17:18:01 +0200

    :construction: Update the script to find the selected folder first, and log the path if that doesn't exist

--- workflows/iterm2/open-iterm-here.sh
+++ workflows/iterm2/open-iterm-here.sh
@@ -3,7 +3,7 @@
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.packageName Iterm2
-# @raycast.title Keksi
+# @raycast.title Open iTerm Here
 # @raycast.mode silent
 
 # Optional parameters:
@@ -15,11 +15,17 @@
 
 osascript<<EOF
     tell application "Finder"
-        if (count of Finder windows) > 0 then
-            set theFolder to (target of front Finder window as alias)
+        set sel to the selection
+        if (count sel) > 0 then
+            set theFolder to item 1 of sel as alias
             return POSIX path of theFolder
         else
-            return "No Finder window open"
+            if (count of Finder windows) > 0 then
+                set theFolder to (target of front Finder window as alias)
+                return POSIX path of theFolder
+            else
+                return "No Finder window open and no folder selected"
+            end if
         end if
     end tell
 EOF
