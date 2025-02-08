8e9b8e6181b71562c118238e282cbef8533da37d
Lnola <nola.luka@gmail.com>
2025-02-08 11:59:03 +0100

    :tada: Create a simple script to create youtube thumbnails

---
+++ scripts/create-yt-thumbnail.py
@@ -0,0 +1,43 @@
+from PIL import Image, ImageDraw, ImageFont
+import random
+import os
+import argparse
+
+
+def generate_color():
+    return tuple(random.randint(0, 255) for _ in range(3))
+
+
+def create_thumbnail(text):
+    img = Image.new("RGB", (1280, 720), generate_color())
+    draw = ImageDraw.Draw(img)
+
+    font_size = 150  # Fixed large font size
+    font_path = (
+        "/System/Library/Fonts/Supplemental/Arial Bold.ttf"  # Default system font
+    )
+
+    try:
+        font = ImageFont.truetype(font_path, font_size)
+    except:
+        font = ImageFont.load_default()
+
+    text_size = draw.textbbox((0, 0), text, font=font)
+    text_width = text_size[2] - text_size[0]
+    text_height = text_size[3] - text_size[1]
+
+    text_x = (1280 - text_width) // 2
+    text_y = (720 - text_height) // 2
+
+    draw.text((text_x, text_y), text, fill=(255, 255, 255), font=font)
+
+    img.save(os.path.join(os.path.expanduser("~"), "Desktop", "thumbnail.png"))
+    img.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate a thumbnail with a title.")
+    parser.add_argument("title", type=str, help="Title text for the thumbnail")
+    args = parser.parse_args()
+
+    create_thumbnail(args.title)
