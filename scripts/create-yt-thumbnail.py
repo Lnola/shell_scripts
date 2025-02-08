1f7f5ca41d516bb54e87e6411f1eac6c7f58495a
Lnola <nola.luka@gmail.com>
2025-02-08 12:11:32 +0100

    🎨 Extract variables to constants

--- scripts/create-yt-thumbnail.py
+++ scripts/create-yt-thumbnail.py
@@ -1,27 +1,30 @@
 from PIL import Image, ImageDraw, ImageFont
 import random, os, argparse
 
+SIZE_X = 1280
+SIZE_Y = 720
+FONT_SIZE = 150
+FONT_PATH = "/System/Library/Fonts/Supplemental/Arial Bold.ttf"
+
 
 def generate_color():
     return tuple(random.randint(0, 255) for _ in range(3))
 
 
-def create_thumbnail(text):
+def create_thumbnail(title):
     img = Image.new("RGB", (1280, 720), generate_color())
     draw = ImageDraw.Draw(img)
-    font_size = 150
-    font_path = "/System/Library/Fonts/Supplemental/Arial Bold.ttf"
 
     try:
-        font = ImageFont.truetype(font_path, font_size)
+        font = ImageFont.truetype(FONT_PATH, FONT_SIZE)
     except:
         font = ImageFont.load_default()
 
-    text_size = draw.textbbox((0, 0), text, font=font)
+    text_size = draw.textbbox((0, 0), title, font=font)
     text_x = (1280 - (text_size[2] - text_size[0])) / 2
-    text_y = (720 - (text_size[3] - text_size[1] + font_size / 2)) / 2
+    text_y = (720 - (text_size[3] - text_size[1] + FONT_SIZE / 2)) / 2
 
-    draw.text((text_x, text_y), text, fill="white", font=font)
+    draw.text((text_x, text_y), title, fill="white", font=font)
     img.save(os.path.join(os.path.expanduser("~"), "Desktop", "thumbnail.png"))
     img.show()
 
