f70ad852ccdd75d00c2d52d040196fd20b598b2d
Lnola <nola.luka@gmail.com>
2025-09-06 15:44:24 +0200

    🔨 Add the raycast shebang

--- scripts/sandbox/run_minerclient.sh
+++ scripts/sandbox/run_minerclient.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.packageName MinerClient
+# @raycast.title Run Miner
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 💎
+
+# Documentation:
+# @raycast.description Open the client for the Miner app.
+# @raycast.author Luka Nola
+
 SANDBOX=~/Sandboxes/MinerClient
 HOME="$SANDBOX" java -Duser.home="$SANDBOX" -jar MinerClient.jar
